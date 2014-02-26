446c1cc2401735a19cbd106271d3eb2c2730a42c
Fred Grott <fred.grott@gmail.com>
2014-02-26 13:10:25 -0600

    added source code to one example cusotmization

--- GWSCustomFontLoaderMAPP/src/gws/grottworkshop/gwscustomfontloadermapp/MainActivity.java
+++ GWSCustomFontLoaderMAPP/src/gws/grottworkshop/gwscustomfontloadermapp/MainActivity.java
@@ -1,5 +1,6 @@
 package gws.grottworkshop.gwscustomfontloadermapp;
 
+import gws.grottworkshop.gwscustomfontloadermapl.FontLoader;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -8,6 +9,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		FontLoader.setDefaultFont(FontLoader.ROBOTO);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
